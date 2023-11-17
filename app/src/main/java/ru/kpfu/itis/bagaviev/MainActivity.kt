348dc83240e876321763ddacd4eb5aeb2292952e
Islam Bagaviev <islam.bagaviev.2014@mail.ru>
2023-11-18 01:55:52 +0300

    Added NavigationComponent and BottomNavigationView

--- app/src/main/java/ru/kpfu/itis/bagaviev/MainActivity.kt
+++ app/src/main/java/ru/kpfu/itis/bagaviev/MainActivity.kt
@@ -2,10 +2,20 @@ package ru.kpfu.itis.bagaviev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = fragment.navController
+
+        val bnvMain = findViewById<BottomNavigationView>(R.id.bnv_main)
+        bnvMain.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/ru/kpfu/itis/bagaviev/fragments/NotificationsFragment.kt
@@ -0,0 +1,28 @@
+package ru.kpfu.itis.bagaviev.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.kpfu.itis.bagaviev.R
+
+class NotificationsFragment : Fragment(R.layout.fragment_notifications) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+}
